f3c62f55c8c9400a64192bc1be1390d7a254b258
Jin <stormjin04@gmail.com>
2023-11-15 00:49:21 -0800

    email

--- components/Card.tsx
+++ components/Card.tsx
@@ -3,9 +3,7 @@ import React, { ReactNode } from 'react'
 function Card({children} : {children : ReactNode}) {
   return (
     <div className='bg-white rounded-md p-2 px-4 pb-1 m- drop-shadow-sm'>
-
         {children}
-
     </div>
   )
 }
